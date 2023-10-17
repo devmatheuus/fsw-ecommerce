27b68dfbac95950ef986dc6a773b965ca0c95d2b
Matheus Silva <matheus_lsilva@hotmail.com>
2023-10-17 15:53:33 -0300

    hotfix: bugs and erros

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -4,11 +4,11 @@ import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { prismaClient } from "./prisma";
 
 export const authOptions: AuthOptions = {
+  adapter: PrismaAdapter(prismaClient),
   providers: [
     GoogleProvider({
-      clientId: process.env.DATABASE_URL,
+      clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
-  adapter: PrismaAdapter(prismaClient),
 };
