f61d4668095079ee300e4793b5c164c2bb6e4403
Matheus Silva <matheus_lsilva@hotmail.com>
2023-10-19 10:58:44 -0300

    feat: add banner to home page

---
+++ src/constants/index.ts
@@ -0,0 +1,6 @@
+// Images import
+import bannerHome01 from "@/assets/banner-home-01.png";
+import bannerHome02 from "@/assets/banner-home-02.png";
+import bannerHome03 from "@/assets/banner-home-03.png";
+
+export { bannerHome01, bannerHome02, bannerHome03 };
