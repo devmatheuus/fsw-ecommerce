75e2f39c0e59e5aa9bb84b208817af92bfaf2089
Matheus Silva <matheus_lsilva@hotmail.com>
2023-10-20 12:06:08 -0300

    feat: ensure recommended products are shown on product details page

--- src/app/product/[slug]/page.tsx
+++ src/app/product/[slug]/page.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import ProductImages from "./components/ProductImages";
 import ProductInfo from "./components/ProductInfo";
 import computedProductTotalPrice from "@/helpers/product";
+import ProductList from "@/components/ui/ProductList";
 
 type ProductDetailsPageProps = {
   params: {
@@ -17,14 +18,28 @@ const ProductDetailsPage: React.FC<ProductDetailsPageProps> = async ({
     where: {
       slug,
     },
+    include: {
+      category: {
+        include: {
+          Product: {
+            where: {
+              slug: {
+                not: slug,
+              },
+            },
+          },
+        },
+      },
+    },
   });
 
   if (!product) return null;
 
   return (
-    <div className="flex flex-col gap-8">
+    <div className="flex flex-col gap-8 pb-8">
       <ProductImages alt={product.name} imageUrls={product.imageUrls} />
       <ProductInfo product={computedProductTotalPrice(product)} />
+      <ProductList products={product.category.Product} />
     </div>
   );
 };
